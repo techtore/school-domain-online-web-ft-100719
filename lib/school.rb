4dc3274c8203eef806614f52869c50c11c1c6534
Victoria Hale <vic.hale74@gmail.com>
2019-10-16 22:55:20 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,14 +7,14 @@ attr_accessor :name, :roster
   @roster = {}
   end
   def add_student(student_name, grade)
-    roster[grade] ||= []
+    roster[grade] ||= [] #if it has a value then use that value otherwise set it equal to whatever is on the right of it.so if there isn't a grade set it equal to an array. the shovel in student name into roster
     roster[grade] << student_name
   end 
-  def grade(grade)
+  def grade(grade) #intance method grade takes in a number(grade) and it's going to return the students in the grarde
     roster[grade]
   end
   def sort
-    roster.each do |grade, name|
+    roster.each do |grade, name| #doesn't need a parameter because it's going to call on the method. could do self.roster instead of @roster. It calls theroster on the instance that got us in here. Self is a context verb 
       roster[grade]= name.sort
     end
   end
